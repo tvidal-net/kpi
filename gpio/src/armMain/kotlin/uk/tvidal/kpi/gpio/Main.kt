89afe12923d1982a8d3717ab007d09a2ec934cc0
thiago <thiago@tvidal.uk>
2019-11-21 21:28:51 +0000

    simple gpio functionality

--- gpio/src/armMain/kotlin/uk/tvidal/kpi/gpio/Main.kt
+++ gpio/src/armMain/kotlin/uk/tvidal/kpi/gpio/Main.kt
@@ -1,4 +1,44 @@
 package uk.tvidal.kpi.gpio
 
+import bcm2835.BCM2835_GPIO_FSEL_INPT
+import bcm2835.BCM2835_GPIO_FSEL_OUTP
+import uk.tvidal.kpi.bcm2835.GPIO
+import uk.tvidal.kpi.getOrDefault
+import uk.tvidal.kpi.u
+
+private fun printHelp() {
+    println(
+        """Usage: gpio.kexe <cmd> [pin]
+- Where <cmd> is one of:
+      in <pin>       configures <pin> as INPUT
+     out <pin>       configures <pin> as OUTPUT
+      on <pin>       changes output of <pin> to HIGH
+     off <pin>       changes output of <pin> to LOW
+    read <pin>       prints HIGH/LOW
+  toggle <pin>       reverses the state
+"""
+    )
+}
+
+private val Array<String>.cmd: String
+    get() = get(0).toLowerCase()
+
+@ExperimentalUnsignedTypes
+private val Array<String>.pin: UInt
+    get() = u(getOrDefault(1, 18))
+
+@ExperimentalUnsignedTypes
 fun main(args: Array<String>) {
+    if (args.isEmpty()) printHelp() else when (args.cmd) {
+        "on" -> GPIO.on(args.pin)
+        "off" -> GPIO.off(args.pin)
+        "in" -> GPIO.mode(args.pin, BCM2835_GPIO_FSEL_INPT)
+        "out" -> GPIO.mode(args.pin, BCM2835_GPIO_FSEL_OUTP)
+        "read" -> println(if (GPIO[args.pin]) "HIGH" else "LOW")
+        "toggle" -> args.pin.let {
+            if (GPIO[it]) GPIO.off(it)
+            else GPIO.on(it)
+        }
+        else -> printHelp()
+    }
 }
